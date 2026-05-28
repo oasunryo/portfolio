3d73b1cea059264c75d550372e9c87a42d67bf5b
sunryo <130203975+oasunryo@users.noreply.github.com>
2026-05-29 08:48:17 +0900

    clean: remove all legacy files to fix vercel build

--- scratch/find_skills_db.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const dump = JSON.parse(fs.readFileSync('scratch/notion_db_dump.json', 'utf8').substring(fs.readFileSync('scratch/notion_db_dump.json', 'utf8').indexOf('[')));
-
-const skills = dump.filter(item => {
-  return item.category.toLowerCase().includes('skill');
-});
-
-console.log("Skills found in DB:", skills.length);
-console.log(JSON.stringify(skills.map(x => ({ title: x.title, category: x.category, org: x.organization, desc: x.description, ind: x.industry })), null, 2));
